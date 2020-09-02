8c0ff68a16153dca76ae3b0f444e8bf003110c63
langat justus <justuslangat78@gmail.com>
2020-09-02 20:14:31 +0300

    write the first test to save the user

--- password_test.py
+++ password_test.py
@@ -6,6 +6,15 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.new_user = User("Langat","justo01","justo1234")
 
+    def test_init(self):
+        self.assertEqual(self.new_user.name,"Langat")
+        self.assertEqual(self.new_user.username,"justo01")
+        self.assertEqual(self.new_user.password,"justo1234")
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
 
 if __name__ == '__main__':
     unittest.main()        
