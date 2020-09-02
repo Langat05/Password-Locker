5da65774f38a6ed197d3e6b1e092b88059f4e885
langat justus <justuslangat78@gmail.com>
2020-09-02 21:04:01 +0300

    write a test to see if we can remove a user from the list

--- password_test.py
+++ password_test.py
@@ -24,6 +24,13 @@ class TestUser(unittest.TestCase):
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)  
 
+    def test_delete_user(self):
+        self.new_user.save_user()
+        test_user = User("Langat","justo01","justo12345") 
+        test_user.save_user()
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list),1)    
+
 
 if __name__ == '__main__':
     unittest.main()        
