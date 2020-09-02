3c85794ec521a0ee8b70d8837787144abfdd2f12
langat justus <justuslangat78@gmail.com>
2020-09-02 20:20:34 +0300

    wrote a teardown method for clean up after each test

--- password_test.py
+++ password_test.py
@@ -15,6 +15,9 @@ class TestUser(unittest.TestCase):
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
 
+    def tearDown(self):
+        User.user_list = []
+
 
 if __name__ == '__main__':
     unittest.main()        
